01fc7d6d82082cc29bfe6713709632d6b1c7a1ee
stephendltg <s.deletang@laposte.net>
2020-05-22 21:52:27 +0200

    text

--- .vuepress/components/TextMagic.vue
+++ .vuepress/components/TextMagic.vue
@@ -37,8 +37,8 @@ export default {
         for(var i=0; i<nodes.length; i++){
           if(scrollUp == true && nodes[i].opacity > 0)
             nodes[i].opacity -= 1/(1+i*1);
-          else if(scrollUp == false && nodes[i].opacity < 1)
-            nodes[i].opacity += 1/(1+i*1);
+          //else if(scrollUp == false && nodes[i].opacity < 1)
+          //  nodes[i].opacity += 1/(1+i*1);
   
           nodes[i].style.opacity = nodes[i].opacity;
         }
